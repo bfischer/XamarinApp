1fdb76de3ccfbd335a7180a03322f72380a1e551
Blake Fischer <blke.fischer@doverals.com>
2016-07-29 15:58:55 -0700

    master: Modified main storyboard. Added additional labels/button to main view controller.

--- XamarinApp.IOS/ViewController.designer.cs
+++ XamarinApp.IOS/ViewController.designer.cs
@@ -1,17 +1,74 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace XamarinApp.IOS
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton AddButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField AmountText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton CancelButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView HotDogImageView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel NameLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel ShortDescriptionLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (AddButton != null) {
+                AddButton.Dispose ();
+                AddButton = null;
+            }
+
+            if (AmountText != null) {
+                AmountText.Dispose ();
+                AmountText = null;
+            }
+
+            if (CancelButton != null) {
+                CancelButton.Dispose ();
+                CancelButton = null;
+            }
+
+            if (HotDogImageView != null) {
+                HotDogImageView.Dispose ();
+                HotDogImageView = null;
+            }
+
+            if (NameLabel != null) {
+                NameLabel.Dispose ();
+                NameLabel = null;
+            }
+
+            if (ShortDescriptionLabel != null) {
+                ShortDescriptionLabel.Dispose ();
+                ShortDescriptionLabel = null;
+            }
         }
     }
 }
